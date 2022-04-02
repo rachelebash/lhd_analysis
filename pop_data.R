f7723f8c6e27d941aa4831227df0a7876b78c43b
Rachel Bash <rbash@lynkertech.com>
2022-04-01 18:34:07 -0600

    add utils folder and population function

--- pop_data.R
+++ pop_data.R
@@ -1,4 +1,19 @@
-source(find_google_drive.R)
-# comment
-# comment
-# lkjljl
+# getting population data
+
+
+
+library(tidycensus)
+library(tidyverse)
+library(tigris)
+
+
+source("misc/find_google_drive.R")
+source("utils/data_utils.R")
+
+
+pop <- get_pop("block group", "P1_001N", "08", "2020")
+
+
+
+
+
