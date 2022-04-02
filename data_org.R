8d8091366af342bc06a5d00deddb73c799b51e94
Rachel Bash <rbash@lynkertech.com>
2022-04-01 18:48:41 -0600

    read in lhd data

--- data_org.R
+++ data_org.R
@@ -11,10 +11,9 @@ library(tigris)
 source("utils/find_google_drive.R")
 source("utils/data_utils.R")
 
+# read in LHD data
+lhd <- read.csv(paste0(drive_dir, "/data/Low Head Dam Inventory Final CIM 092920 - Inventory.csv"))
 
+#get population data for Colorado by census block
 pop <- get_pop("block group", "P1_001N", "08", "2020")
-
-
-
-
 
